11ec050f6322f15e11020b69bae26e0f9a653842
Thomas Bechtold <thomasbechtold@jpberlin.de>
2026-02-21 15:29:30 +0100

    feat: add cli for isbj parser to be able to debug Abrechnungen

---
+++ cmd/isbj/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/eenemeene/kitamanager-go/internal/isbj"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: isbj <path-to-xlsx>\n")
+		os.Exit(1)
+	}
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "open: %v\n", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	raw, err := isbj.ParseFromReader(f)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "parse: %v\n", err)
+		os.Exit(1)
+	}
+
+	converted, err := isbj.Convert(raw)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "convert: %v\n", err)
+		os.Exit(1)
+	}
+
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ")
+	if err := enc.Encode(converted); err != nil {
+		fmt.Fprintf(os.Stderr, "json: %v\n", err)
+		os.Exit(1)
+	}
+}
